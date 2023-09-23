d6dea8bee60e698eec4265f1797f28e2bef8d378
Hantz1 <jeffhantzbelizaire604@gmail.com>
2023-09-23 17:04:18 -0400

    3e commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import Header from './components/header/header';
+import Sidebar from './components/sidebar/sidebar';
 import './utils/style/App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='header'>
+        <Header/>
+      </div>
+      <div className='sidebar'>
+        <Sidebar/>
+      </div>
+      <div className='main'>
+        main
+      </div>
     </div>
   );
 }
--- src/utils/style/GlobalStyle.jsx
+++ src/utils/style/GlobalStyle.jsx
@@ -6,7 +6,12 @@ const StyledGlobalStyle = createGlobalStyle`
     }
 
     body{
+        font-family: 'Montserrat Regular';
         background-color: #f9f9f9;
+        src: local('Montserrat Regular'), url('../src/assets/fonts/Montserrat-Regular.ttf') format ('ttf');
+        src: local('Montserrat Black'), url('../src/assets/fonts/Montserrat-Black.ttf') format ('ttf');
+        src: local('Montserrat Bold'), url('../src/assets/fonts/Montserrat-Bold.ttf') format ('ttf');
+        src: local('Montserrat ExtraBold'), url('../src/assets/fonts/Montserrat-ExtraBold.ttf') format ('ttf');
     }
 `
 
